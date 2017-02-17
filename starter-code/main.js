ee641dabb2a219277455353f9583948dc17d932a
gchoy <gabriela.choy@gmail.com>
2017-02-16 20:24:55 -0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,17 @@
 //console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+//var cardOne = "queen";
+//var cardTwo = "king";
+//var cardThree = "queen";
+//var cardFour = "king";
 
+var createCards = function(){
+	var gameBoard = document.getElementById("game-board");
+	for (var i=0; i<4 ;i++){
+		var div = document.createElement('div');
+		div.className = "card";
+		gameBoard.appendChild(div);
+	}
+}
+
+createCards();
